c4f0e4bb5f3f0f66f929fc8b1b91482b518d1615
ketan <ketansahils@gmail.com>
2016-10-25 17:16:24 +0530

    Add ans to Ques 2

--- projects/ti_practice/solutions.py
+++ projects/ti_practice/solutions.py
@@ -1,13 +1,13 @@
 def question1(s='',t=''):
-	text = s.split(' ')
-	pat = t.split(' ')
+	text = s.split(' ') if s is not None else ['']
+	pat = t.split(' ') if t is not None else ['']
 	s = str.lower(''.join(text))
 	t = str.lower(''.join(pat))
 	counter = 0
 	arr = [0] * 26
 
 	for char in t:
-		if 97 <= ord(char) <= 122:
+		if ord(char) > 122 or ord(char) < 97:
 			continue
 		index = ord(char) - 97
 		arr[index] += 1
@@ -27,8 +27,41 @@ def question1(s='',t=''):
 		return True
 	return False			
 
-def question2():
-	pass
+def question2(a):
+	n = len(a)
+	longest = [0,0]
+
+	def checker(x,y):
+		temp = [0,0]
+		while x >= 0 and y < n:
+			#print "Comparing",a[x],"and",a[y]
+			#print "temp",a[temp[0]:temp[1]+1]
+			if not a[x].isalpha():
+				x = x-1
+			elif not a[y].isalpha():
+				y = y+1
+			elif a[x] == a[y]:
+				temp[0] = x
+				temp[1] = y
+				x,y = x-1, y+1
+			else:
+				break
+		if temp[1] - temp[0] > longest[1] - longest[0]:
+			longest[0], longest[1] = temp[0], temp[1]
+		return
+
+	for i in range(1,n):
+		#print "i:",i,"array:",a[longest[0]:longest[1]+1]
+		checker(i-1,i)
+
+	if not sum(longest) == n - 1:
+		#print "2nd loop"
+		for j in range(1,n-1):
+			#print "j:",j,"array:",a[longest[0]:longest[1]+1]
+			checker(j-1,j+1)
+	return a[longest[0]:longest[1]+1]
+
+
 	
 def question3():
 	pass
@@ -39,8 +72,28 @@ def question4():
 def question5():
 	pass
 
+############## QUESTION 1 ##############
+# s1 = "Udacity ketaan"
+# t = "ekyt"
+# 
+# t1 = ""
+# t2 = None
+# t3 = "ana"
+# t4 = "ektanu"
+# 
+# T = [t,t1,t2,t3,t4]
+# for item in T:
+# 	print question1(s1,item)
 
-s = "Udacity ketan"
-t = "ekyt"
+# True
+# True
+# True
+# True
+# False
+########################################
 
-print question1(s,t)
+############## QUESTION 2 ##############
+print question2('nurses run')
+# nurses run
+print question2('asma da m ')
+# ma da m
